252f2099138ee4e10e262fa8f7043add0742a281
Jimmy Domagala-Tang <jdomaga@uwo.ca>
2017-11-18 20:50:53 -0600

    completed test posts

--- testClientCode/testscipts.js
+++ testClientCode/testscipts.js
@@ -1,11 +1,16 @@
-function whytho(){ 
-// 	$.get("http:localhost:8080" , function( data ) {
-// 		alert();
-//   $( ".result" ).html( data );
-//   alert( "Load was performed." );
-// });
- 
+function compare(a,b) {
+  if (a.score < b.score)
+    return 1;
+  if (a.score > b.score)
+    return -1;
+  return 0;
+}
+
+
+function whytho(event){  
+ 	event.preventDefault();
 	var formData = new FormData($('#uploadForm')[0]);
+	        var obj =[]; 
 	$.ajax({
 	    url: "http:localhost:8080/api",
 	    type: "POST",
@@ -15,15 +20,29 @@ function whytho(){
 	    cache: false,
 	    data: formData,
 	    success: function(response) {
-	        alert("success");
-	        alert(response);
+	        alert("success"); 
+	        var js = JSON.parse(response); 
+	        var allresults = js.images[0].classifiers[0].classes; 
+	        	for(var i=0; i<allresults.length; i++){ 
+	        		obj.push({score: allresults[i].score, prodName : allresults[i].class });  
+	        	}   
+	       obj.sort(compare); 
+	       for(var j = 0; j<obj.length; j++){
+	       		alert("score is: " + obj[j].score + " , name is: " + obj[j].prodName);
+	       } 
 	    },
 	    error: function(err) {
-	    	alert("sadeface");
-	        // alert(err.responseText);
+	    	alert("no"); 
 	    }
-	}); 
-	alert();
+	});  
 }
 
+
+function tryme(){
+	$.get("https://backflipp.wishabi.com/flipp/items/search?locale=en-ca&postal_code=N6G1A1&sid=4296250855351882&q=lays", 
+		function(data){ 
+			// alert(data);
+			$("#update").html(Object.keys()); 
+	})		
+}
 
